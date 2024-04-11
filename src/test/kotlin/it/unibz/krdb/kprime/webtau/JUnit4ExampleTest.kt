d47676c394e50bfd505d721a79d54e5cfc40c0b9
npedot <nicola.pedot@gmail.com>
2024-04-11 18:23:48 +0200

    adds webtau test library

---
+++ src/test/kotlin/it/unibz/krdb/kprime/webtau/JUnit4ExampleTest.kt
@@ -0,0 +1,14 @@
+package it.unibz.krdb.kprime.webtau
+
+import org.junit.runner.RunWith
+import org.testingisdocumenting.webtau.junit4.WebTauRunner
+import kotlin.test.Test
+
+@RunWith(WebTauRunner::class)
+class JUnit4ExampleTest {
+
+    @Test
+    fun test_webTauRunner() {
+
+    }
+}
